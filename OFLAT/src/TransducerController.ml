b7c379720659f518a77112a71adfac3b5b078cf1
Joao <jb.santos@campus.fct.unl.pt>
2026-01-03 13:49:10 +0000

    begin work on frontend and library cleanup

---
+++ OFLAT/src/TransducerController.ml
@@ -0,0 +1,9 @@
+open AutomatonController
+open TransducerView
+
+class fstController (fst : TransducerView.model) (s: bool) =
+  object (self) inherit automatonController(s) as super
+
+  
+end
+  
---
+++ OFLAT/src/TransducerView.ml
@@ -0,0 +1,8 @@
+open OCamlFlat
+open BasicTypes
+open AutomatonView
+
+module TransducerView = 
+struct
+	open Transducer
+end
